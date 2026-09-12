14a9a1027f9fdba958efd6b2dbf741ac63af9492
Igor Benavente <ib.benavente@gmail.com>
2026-09-12 18:42:56 -0300

    context window implementation

--- m3/src/contextWindow.ts
+++ m3/src/contextWindow.ts
@@ -2,7 +2,14 @@ import { Message } from "./types.ts";
 
 export class ContextWindow {
     private history: Message[] = [];
+    private maxMessages: number;
+
+    constructor (maxHistoryLength: number = 10) {
+        this.maxMessages = maxHistoryLength;
+    }
+
     
+
     addUserMessage(content: string) {
         this.history.push({ role: "user", content });
     }
@@ -12,6 +19,13 @@ export class ContextWindow {
     }
 
     getMessages(){
+        const limit = this.maxMessages * 2;
+        if (this.history.length <= limit) {
+            this.history = this.history.slice(-limit);
+            //para manter o limite de memoria na aplicação porem com mais uso de memoria e cpu
+            //poderiamos usar algum banco de dados para salvar o contexto e não disperdica-lo, mas para o escopo do projeto vamos manter em memoria
+            // return this.history.slice(-limit);
+        }
         return this.history;
     }
 }
